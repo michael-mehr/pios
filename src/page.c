2e91f58ee4e5a782250a2999625da3c5990ccd67
Michael <michaelmehr0@gmail.com>
2024-10-01 18:56:55 -0500

    page.c function prototypes

---
+++ src/page.c
@@ -0,0 +1,19 @@
+
+// page.c
+
+#include "page.h"
+#include "list.c"
+
+struct ppage physical_page_array[128]; // 128 pages, each 2mb in length covers 256 megs of memory
+
+void init_pfa_list(void) {
+  
+}
+
+struct ppage *allocate_physical_pages(unsigned int npages) {
+
+}
+
+void free_physical_pages(struct ppage *ppage_list) {
+  
+}
