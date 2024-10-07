857fe24df619f37a35ac27c97ea25b41b5c50f96
Michael <michaelmehr0@gmail.com>
2024-10-07 14:20:25 -0500

    list_add()

--- src/page.c
+++ src/page.c
@@ -6,7 +6,10 @@
 
 struct ppage physical_page_array[128]; // 128 pages, each 2mb in length covers 256 megs of memory
 
+
+
 void init_pfa_list(void) {
+  unsigned int arraySize = sizeof(physical_page_array) / sizeof(physical_page_array[0]);
   
 }
 
@@ -15,5 +18,5 @@ struct ppage *allocate_physical_pages(unsigned int npages) {
 }
 
 void free_physical_pages(struct ppage *ppage_list) {
-  
+
 }
