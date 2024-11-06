4d77601350d98a48f9d70abe584a2c21a7fa0baa
Michael <michaelmehr0@gmail.com>
2024-11-06 00:43:10 -0600

    serial.c and serial.h

--- src/serial.c
+++ src/serial.c
@@ -1,7 +1,15 @@
 // serial.c
 
+#include "serial.h"
+#include "rprintf.h"
+
 // writes character to serial port
 void putc(int data) {
-  int *mu_io = 0x3F215040; // MU_IO register
+  int *mu_io = (int *)MU_IO_REG;
   *mu_io = data;
+}
+
+// writes string to serial port
+void uart_puts(char *str) {
+  esp_printf(putc, str);
 }
--- src/serial.h
+++ src/serial.h
@@ -1,3 +1,6 @@
 // serial.h
 
+#define MU_IO_REG 0x3F215040
+
 void putc(int data);
+void uart_puts(char *str);
