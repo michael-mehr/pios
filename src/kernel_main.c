fa789c6f2a840d31ec07d8eb9af1583685b85bfc
Michael <michaelmehr0@gmail.com>
2024-11-10 18:32:57 -0600

    update

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -51,15 +51,15 @@ void kernel_main() {
   esp_printf(putc, "Current Execution Level is %d\r\n", getEL()); // Uses putc to print a string
 
   // page frame allocator
-  // init_pfa_list();
-  // struct ppage *allocd_list = allocate_physical_pages(10);
+  init_pfa_list();
+  struct ppage *allocd_list = allocate_physical_pages(10);
   // free_physical_pages(allocd_list);
 
   // MMU Setup
   extern struct table_descriptor_stage1 L1table;
   extern struct page_descriptor_stage1 L2table;
   struct table_descriptor_stage1 *L1table_ptr = &L1table;
-  loadPageTable(L1table_ptr);
+  // loadPageTable(L1table_ptr);
   // mapPages(0x100000, 0x200000); // map virtual address 0x100000 to physical address 0x200000
 
 
