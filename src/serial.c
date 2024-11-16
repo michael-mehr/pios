1feca8f5bed1c4be831b9a32d5bc41eeb161dab6
Michael <michaelmehr0@gmail.com>
2024-11-16 14:48:23 -0600

    serial.c

--- src/serial.c
+++ src/serial.c
@@ -1,14 +1,14 @@
 // serial.c
 
-int *mu_io = 0x3F215040; // MU_IO register
-
 // writes character to serial port
 void putc(int data) {
+  int *mu_io = 0x3F215040; // MU_IO register
   *mu_io = data;
 }
 
 // reads character from serial port
 char getc() {
+  int *mu_io = 0x3F215040; // MU_IO register
   return *mu_io;
 }
 
